91b7219e9dea4311602b0d0f64718a00e794535d
AfroMonkey <moisalejandro@gmail.com>
2017-05-19 18:13:19 -0500

    [MYB]

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -1,51 +1,29 @@
-from openerp import api, models
+from openerp import api, fields, models
 
 
 class MRPProduction(models.Model):
     _inherit = 'mrp.production'
 
-    @api.model
-    def _get_default_location_src_id(self):
-        location = False
-        if self.bom_id.location_src_id:
-            location = self.bom_id.location_src_id
-        else:
-            if self._context.get('default_picking_type_id'):
-                location = self.env['stock.picking.type'].browse(self.env.context['default_picking_type_id']).default_location_src_id
-            if not location:
-                location = self.env.ref('stock.stock_location_stock', raise_if_not_found=False)
-        return location and location.id or False
+    def _src_id_default(self):
+        return self.bom_id.location_src_id and self.bom_id.location_src_id.id or False
 
-    @api.model
-    def _get_default_location_dest_id(self):
-        location = False
-        if self.bom_id.location_src_id:
-            location = self.bom_id.location_src_id
-        else:
-            if self._context.get('default_picking_type_id'):
-                location = self.env['stock.picking.type'].browse(self.env.context['default_picking_type_id']).default_location_dest_id
-            if not location:
-                location = self.env.ref('stock.stock_location_stock', raise_if_not_found=False)
-        return location and location.id or False
+    def _dest_id_default(self):
+        return self.bom_id.location_src_id and self.bom_id.location_src_id.id or False
 
-    @api.onchange('bom_id')
-    def _set_locations(self):
-        location = False
-        if self.bom_id.location_src_id:
-            location = self.bom_id.location_src_id
-        else:
-            if self._context.get('default_picking_type_id'):
-                location = self.env['stock.picking.type'].browse(self.env.context['default_picking_type_id']).default_location_src_id
-            if not location:
-                location = self.env.ref('stock.stock_location_stock', raise_if_not_found=False)
-        self.location_src_id = location and location.id or False
+    location_src_id = fields.Many2one(
+        'stock.location',
+        string='Raw Materials Location',
+        required=True,
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        help="Location where the system will look for components.",
+        default=_src_id_default)
 
-        location = False
-        if self.bom_id.location_src_id:
-            location = self.bom_id.location_src_id
-        else:
-            if self._context.get('default_picking_type_id'):
-                location = self.env['stock.picking.type'].browse(self.env.context['default_picking_type_id']).default_location_dest_id
-            if not location:
-                location = self.env.ref('stock.stock_location_stock', raise_if_not_found=False)
-        self.location_dest_id = location and location.id or False
+    location_dest_id = fields.Many2one(
+        'stock.location',
+        string='Raw Materials Location',
+        required=True,
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        help="Location where the system will look for components.",
+        default=_dest_id_default)
