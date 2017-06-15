b8892ac7a35c87c5aef49d3859414ea93940ba0b
AfroMonkey <moisalejandro@gmail.com>
2017-06-15 14:36:34 -0500

    [FIX] Error if no location in bom

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -5,7 +5,7 @@ class MRPProduction(models.Model):
     _inherit = 'mrp.production'
 
     def _make_production_produce_line(self, cr, uid, production, context=None):
-        production.location_src_id = production.bom_id.location_src_id.id or production.product_id.property_stock_production.id
-        production.location_dest_id = production.bom_id.location_dest_id.id or production.location_dest_id.id
+        production.location_src_id = production.bom_id.location_src_id and production.bom_id.location_src_id.id or production.product_id.property_stock_production.id
+        production.location_dest_id = production.bom_id.location_dest_id and production.bom_id.location_dest_id.id or production.location_dest_id.id
         vals = super(MRPProduction, self)._make_production_produce_line(cr, uid, production, context)
         return vals
